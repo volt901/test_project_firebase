a4435bbae865fc77b8a99784692a02cf57cab9eb
vladimir <volt901@icloud.com>
2022-01-08 18:04:32 +0500

    Initial Commit

---
+++ test_project_firebase/test_project_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_project_firebaseApp.swift
+//  test_project_firebase
+//
+//  Created by Владимир Ширяев on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct test_project_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
